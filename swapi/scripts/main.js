fc50665ab5778b73485fc6c40c934b4688c6d4ed
DmitriyYaroshchuk <dima030215@gmail.com>
2023-04-26 20:53:17 +0300

    task swapi test-2

--- swapi/scripts/main.js
+++ swapi/scripts/main.js
@@ -1,26 +1,64 @@
-import { fulfillRequest } from "./module";
+import { fulfillRequest } from "./module.js";
 
 function mainFunc( options ) {
-    const { spanUrl, inputUrl, button} = options;
-
+    const { spanUrl, inputUrl, button, idBlock, controllerBlock, resultBlock, loader } = options;
     button.addEventListener('click', async (event) => {
         event.preventDefault();
         const { fulfillRequest } = await import('./module.js');
-        const regExp = '(/[a-z]+/\d+/$|/\//)';
-        const validateInput = regExp.test(inputUrl);
+        const fixInputUrl = inputUrl.value.trim();
+        // console.log(typeof fixInputUrl, fixInputUrl);
+        const regExp = /^\/[a-z]*\/?\d*\/?$/;
+        const validateInput = regExp.test(fixInputUrl);
+        // console.log('inputUrl:', inputUrl);
+        // console.log('validateInput:', validateInput);
         if (validateInput) {
-            const response = fulfillRequest(spanUrl.textContent, inputUrl.value);
-            console.log('Response: ', response)
+            loader.style.display = 'block';
+            const response = await fulfillRequest(spanUrl.textContent, fixInputUrl);
+            // console.log('Response: ',response);
+            // console.log('Response-data: ', (await response).data);
+            // console.log('withoutTrim:', inputUrl.value, inputUrl.value.length);
+            // console.log('Trim:', inputUrl.value.trim(), inputUrl.value.trim().length);
+            if ((await response).status === 'success') {
+                loader.style.display = 'none';
+                const parseUrl = fixInputUrl.split('/');
+                // console.log('parseUrl:', parseUrl);
+                controllerBlock.style.display = 'block';
+                idBlock.style.display = 'block'
+                controllerBlock.innerHTML = parseUrl[1];
+                idBlock.innerHTML = parseUrl[2];
+                resultBlock.innerHTML = JSON.stringify((await response).data, null, 2);
+                console.log(fixInputUrl)
+                if (fixInputUrl === '/') {
+                    controllerBlock.style.display = 'none';
+                    idBlock.style.display = 'none'
+                }
+            }
+            else {
+                controllerBlock.style.display = 'none';
+                idBlock.style.display = 'none';
+            }
         }
-
-
-
+        else {
+            console.error('Please check again the correctness of the data of the field');
+        }
     })
 }
-mainFunc({
-    spanUrl: document.querySelector('.js--url-span'),
-    inputUrl: document.querySelector('.js--url-input'),
-    button: document.querySelector('.js--button-request'),
-});
+try {
+    document.addEventListener('DOMContentLoaded', () => {
+        mainFunc({
+            spanUrl: document.querySelector('.js--url-span'),
+            inputUrl: document.querySelector('.js--url-input'),
+            button: document.querySelector('.js--button-request'),
+            idBlock: document.querySelector('.js--id-swapi'),
+            controllerBlock: document.querySelector('.js--controller-swapi'),
+            resultBlock: document.querySelector('.js--code'),
+            loader: document.querySelector('.js--loader'),
+        })
+    })
+} catch (error) {
+    console.error(error);
+}
+
+
 
 
--- swapi/scripts/module.js
+++ swapi/scripts/module.js
@@ -2,12 +2,13 @@ export async function fulfillRequest (spanUrl, inputUrl) {
     const response = await fetch(`${spanUrl}/${inputUrl}`, {
         method: 'GET'
     });
-   try {
+    try {
        return {
            status: 'success',
            data: await response.json()
        }
-   } catch {
+   } catch (error) {
+       console.error('It failed to get a response from the server, check the data', error);
        return {
            status: 'error',
            data: null
