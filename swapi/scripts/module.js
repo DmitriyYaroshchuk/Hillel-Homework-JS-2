e28350cd3b9c2becc1fb15f3ba24a1d8269d54a2
DmitriyYaroshchuk <dima030215@gmail.com>
2023-04-26 23:33:06 +0300

    task swapi initial commit

--- swapi/scripts/module.js
+++ swapi/scripts/module.js
@@ -2,13 +2,14 @@ export async function fulfillRequest (spanUrl, inputUrl) {
     const response = await fetch(`${spanUrl}/${inputUrl}`, {
         method: 'GET'
     });
+    if (!response.ok) console.error('Something has happened');
     try {
        return {
            status: 'success',
            data: await response.json()
        }
    } catch (error) {
-       console.error('It failed to get a response from the server, check the data', error);
+       console.error(error);
        return {
            status: 'error',
            data: null
