3527d42e51cd1796e5cbb8190c0f8cf24184cc17
DmitriyYaroshchuk <dima030215@gmail.com>
2023-06-06 14:56:06 +0300

    task todo-func fix node modules

--- todo-form/app-todo-form/src/ui/components/form/TodoForm/TodoForm.js
+++ todo-form/app-todo-form/src/ui/components/form/TodoForm/TodoForm.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Form } from "react-final-form";
 import Login from "../Login/Login";
 
+
 export default function TodoForm(props) {
 
     const { handleAdd, inputRef } = props;
     return (
         <Form
             onSubmit={handleAdd}
-            render={(props) => <Login {...props} inputRef={inputRef} />}
+            render={() => <Login handleAdd={handleAdd} inputRef={inputRef} />}
         />
-
     )
 
 }
+
