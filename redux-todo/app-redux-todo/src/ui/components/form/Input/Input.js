c3f83b7f0851cd96c9eebc7a556fdd5c0ed29244
DmitriyYaroshchuk <dima030215@gmail.com>
2023-06-26 23:41:14 +0300

    redux-todo is completed

--- redux-todo/app-redux-todo/src/ui/components/form/Input/Input.js
+++ redux-todo/app-redux-todo/src/ui/components/form/Input/Input.js
@@ -6,14 +6,12 @@ export default function Input(props) {
     const { text, type, name, customClass, meta, input } = props;
     const classes = useStyles(props);
     const className = cx(`${classes['form__input']}`, { [customClass]: customClass });
-    // const error = (meta) => meta.error && meta.touched
     return (
         <>
             <input value={text}
                    type={type}
                    name={name}
                    className={className}
-                   // onError={error}
                    {...input}
             />
             {meta.error && meta.touched && !meta.pristine && <span style={{ fontWeight: 400, color: 'red' }}>{meta.error}</span>}
